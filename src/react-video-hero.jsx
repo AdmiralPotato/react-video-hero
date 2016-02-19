17d5f27a0f8f1654ae91cb8a03a8a109d768ab22
Admiral Potato <admiral@nuclearpixel.com>
2016-02-19 11:59:32 -0700

    YESSSS ASPECT RATIO

--- src/react-video-hero.jsx
+++ src/react-video-hero.jsx
@@ -3,15 +3,89 @@ var ReactDOM = require('react-dom');
 
 
 var VideoHero = React.createClass({
+	getInitialState(){
+		return {
+			width: 0,
+			height: 0,
+			needsResizeUpdate: true
+		};
+	},
+	componentDidMount(){
+		var slider = this;
+		window.addEventListener('resize', slider.invalidateDimensions);
+		slider.updateDimensions();
+		slider.setState({
+			resizeFallbackIntervalId: setInterval(() => {
+				slider.fallbackStateUpdate();
+			}, 200)
+		});
+	},
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.invalidateDimensions);
+		this.updateDimensions();
+		clearInterval(this.state.resizeFallbackIntervalId);
+	},
+	invalidateDimensions(){
+		this.setState({
+			needsResizeUpdate: true
+		});
+	},
+
+	fallbackStateUpdate(){
+		var slider = this;
+		var holder = ReactDOM.findDOMNode(slider.refs['holder']);
+		var holderWidth = holder ? holder.clientWidth : 0;
+		var holderHeight = holder ? holder.clientHeight : 0;
+		var holderWidthHasChanged = slider.state.holderWidth !== holderWidth;
+		var holderHeightHasChanged = slider.state.holderHeight !== holderHeight;
+		if (holderWidthHasChanged || holderHeightHasChanged) {
+			slider.invalidateDimensions();
+			slider.updateDimensions();
+		}
+	},
+
+	updateDimensions(){
+		var slider = this;
+		//reference: http://stackoverflow.com/questions/19014250/reactjs-rerender-on-browser-resize
+		if (slider.state.needsResizeUpdate) {
+			var holder = ReactDOM.findDOMNode(slider.refs['holder']);
+			var holderWidth = holder ? holder.clientWidth : 0;
+			var holderHeight = holder ? holder.clientHeight : 0;
+			slider.setState({
+				width: holderWidth,
+				height: holderHeight,
+				needsResizeUpdate: false
+			});
+		}
+	},
+
 	render(){
 		var props = this.props;
 		var sourceList = props.sources.map((source, index) => {
 			return (<source key={index} type={source.type} src={source.src}/>);
 		});
+		var ratioStyle = {};
+		var videoRatio = 9 / 16;
+		var holderRatio = this.state.height / this.state.width;
+		if(videoRatio < holderRatio){
+			ratioStyle.height = this.state.height;
+		} else {
+			ratioStyle.width = this.state.width;
+		}
+		var videoProps = {
+			className: aspect,
+			poster: props.poster,
+			preload: "auto",
+			autoPlay: "autoPlay",
+			muted: "muted",
+			loop: "loop",
+			style: ratioStyle
+		};
+
 		var aspect = props.aspect || 'aspect-16_9';
 		return (
-			<div className={"VideoHero " + aspect}>
-				<video poster={props.poster} preload="auto" autoPlay="autoPlay" muted="muted" loop="loop" webkit-playsinline="">
+			<div className="VideoHero" ref="holder">
+				<video {...videoProps} webkit-playsinline="">
 					{sourceList}
 				</video>
 				<div className="children">
